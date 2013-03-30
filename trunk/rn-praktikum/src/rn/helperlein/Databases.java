e72beadab6da6fdf073eb97658d329fbafa43d89
breitenstein.alexander@googlemail.com <breitenstein.alexander@googlemail.com@b347f7b3-2220-c1e4-933a-b99727ceb3eb>
2013-03-30 17:03:54 +0000

    rnp1: added user- and mail-Provider, added help Class Databases for connection via SQLite.db

---
+++ trunk/rn-praktikum/src/rn/helperlein/Databases.java
@@ -0,0 +1,45 @@
+package rn.helperlein;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ * User: Alex
+ * Date: 30.03.13
+ * Time: 12:06
+ */
+public class Databases {
+
+    /**
+     *
+     * @param databaseName
+     * @return
+     */
+    public static Connection initializeSQLiteConnection(String databaseName) {
+
+        Connection connection = null;
+
+        try {
+            Class.forName("org.sqlite.JDBC");
+            //Alex
+            String url = "jdbc:sqlite:D:/Alex/HAW/AI-4/RN/rn-praktikum/"+ databaseName +".db";
+
+            //Sven
+//            String url;
+
+
+            //HAW
+//            String url;
+
+            connection = DriverManager.getConnection(url);
+
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+        } catch (SQLException e) {
+            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+        }
+
+        return connection;
+    }
+}
