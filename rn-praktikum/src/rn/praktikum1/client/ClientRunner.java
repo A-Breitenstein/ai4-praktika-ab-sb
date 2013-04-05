ec53dcb6d02b7019b5850c0cd39fc4972523088d
sven.bartel90 <sven.bartel90@b347f7b3-2220-c1e4-933a-b99727ceb3eb>
2013-04-05 14:04:57 +0000

    rnp: praktikum1 clientRunner ohne uidl

--- rn-praktikum/src/rn/praktikum1/client/ClientRunner.java
+++ rn-praktikum/src/rn/praktikum1/client/ClientRunner.java
@@ -4,12 +4,16 @@ import rn.helperlein.Communication;
 import rn.praktikum1.server.mails.Message;
 import rn.praktikum1.server.mails.User;
 import rn.praktikum1.server.states.Messages;
+import sun.reflect.generics.tree.FieldTypeSignature;
 
-import java.io.BufferedReader;
-import java.io.DataOutputStream;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.net.Socket;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 
 /**
  * User: Alex
@@ -18,65 +22,82 @@ import java.net.Socket;
  */
 public class ClientRunner implements Runnable {
 
-    UserDescriptor userDescriptor;
-    Socket socket;
+    private UserDescriptor userDescriptor;
+    private Socket socket;
     private DataOutputStream outToServer; // Ausgabestream zum Server
     private BufferedReader inFromServer;  // Eingabestream vom Server
+    private FileWriter fileWriter;
 
     public ClientRunner(UserDescriptor userDescriptor) {
         this.userDescriptor = userDescriptor;
-        try {
-            socket = new Socket(userDescriptor.getServerIp(), userDescriptor.getServerPort());
-            outToServer = new DataOutputStream(socket.getOutputStream());
-            inFromServer = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
     }
 
 
     @Override
     public void run() {
+        DateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
 
-        try {
-            String response = readFromServer();
-            if (response.equals(Messages.OK)) {
-                writeToServer("USER " + userDescriptor.getUser().getUsername());
-                response = readFromServer();
-                if (response.equals(Messages.OK)) {
-                    writeToServer("PASS " + userDescriptor.getUser().getPassword());
+        while (true) {
+
+            try {
+                socket = new Socket(userDescriptor.getServerIp(), userDescriptor.getServerPort());
+                outToServer = new DataOutputStream(socket.getOutputStream());
+                inFromServer = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                fileWriter = new FileWriter(userDescriptor.getUser().getUsername() + ".txt", true);
+                Calendar cal = Calendar.getInstance();
+
+                String response = readFromServer();
+                if (response.contains(Messages.OK)) {
+                    writeToServer("USER " + userDescriptor.getUser().getUsername());
                     response = readFromServer();
-                    if (response.equals(Messages.OK)) {
-                        writeToServer("STAT");
+                    if (response.contains(Messages.OK)) {
+                        writeToServer("PASS " + userDescriptor.getUser().getPassword());
                         response = readFromServer();
-                        String[] parts = response.trim().split(" ");
-                        int message_count = Integer.valueOf(parts[1]);
-                        for (int i = 1; i <= message_count; i++) {
-
-                            writeToServer("RETR " + i);
+                        if (response.contains(Messages.OK)) {
+                            writeToServer("STAT");
                             response = readFromServer();
-                            if (response.equals(Messages.OK)) {
-                                while (!response.equals(".")) {
-                                    response = readFromServer();
-                                    System.out.println(response);
+                            String[] parts = response.trim().split(" ");
+                            int message_count = Integer.valueOf(parts[1]);
+                            for (int i = 1; i <= message_count; i++) {
+
+                                writeToServer("RETR " + i);
+                                response = readFromServer();
+                                if (response.contains(Messages.OK)) {
+                                    fileWriter.write("############ "+dateFormat.format(cal.getTime())+" ############## \r\n");
+                                    while (!response.equals(".")) {
+                                        response = readFromServer();
+                                        fileWriter.write(response+"\r\n");
+                                        fileWriter.flush();
+
+                                    }
                                 }
                             }
-                        }
 
+                            writeToServer("QUIT");
+                            response = readFromServer();
+                            if (response.contains(Messages.OK)) {
+                                outToServer.close();
+                                inFromServer.close();
+                                socket.close();
+                                fileWriter.close();
+                            }
+
+                        }
                     }
                 }
-            }
 
-            while (true) {
-                Thread.sleep(1000);
+                Thread.sleep(30000);
+
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
             }
-        } catch (InterruptedException e) {
-            e.printStackTrace();
         }
     }
 
     public static void main(String[] args) {
-        new ClientRunner(UserDescriptor.create(User.create("user1", "pw1"), "127.0.0.1", 11000)).run();
+        new ClientRunner(UserDescriptor.create(User.create("user2", "pw2"), "127.0.0.1", 11000)).run();
     }
 
     private void writeToServer(String request) {
