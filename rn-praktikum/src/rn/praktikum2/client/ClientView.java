520a2aa2c2f79b336227635122ba8fc9ba9e6007
breitenstein.alexander <breitenstein.alexander@googlemail.com>
2013-04-19 00:12:47 +0000

    rnp2: eingabe von text wird validiert und button nach inhalt des edit feldes enabled/disabled, absenden per 'enter' und per button mit einfügen ins chatwindow funktionieren

--- rn-praktikum/src/rn/praktikum2/client/ClientView.java
+++ rn-praktikum/src/rn/praktikum2/client/ClientView.java
@@ -1,7 +1,15 @@
 package rn.praktikum2.client;
 
 import javax.swing.*;
+import javax.swing.event.DocumentEvent;
+import javax.swing.event.DocumentListener;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.InputMethodEvent;
+import java.awt.event.InputMethodListener;
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
 
 /**
  * Created with IntelliJ IDEA.
@@ -17,6 +25,57 @@ public class ClientView extends JFrame {
     private JButton btn_send;
     private JTextArea w_userWindow;
 
+
+    public ClientView() {
+        e_editFieldChat.getDocument().addDocumentListener(new DocumentListener() {
+            @Override
+            public void insertUpdate(DocumentEvent e) {
+                validateButton(e_editFieldChat);
+            }
+
+            @Override
+            public void removeUpdate(DocumentEvent e) {
+                validateButton(e_editFieldChat);
+            }
+
+            @Override
+            public void changedUpdate(DocumentEvent e) {
+                validateButton(e_editFieldChat);
+            }
+        });
+        e_editFieldChat.addPropertyChangeListener(new PropertyChangeListener() {
+            @Override
+            public void propertyChange(PropertyChangeEvent evt) {
+                validateButton(e_editFieldChat);
+            }
+        });
+        e_editFieldChat.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String text = e_editFieldChat.getText();
+                w_chatWindow.append(text+"\n");
+                e_editFieldChat.setText("");
+            }
+        });
+        btn_send.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String text = e_editFieldChat.getText();
+                w_chatWindow.append(text+"\n");
+                e_editFieldChat.setText("");
+            }
+        });
+    }
+
+    private void validateButton(JTextField textField){
+        String editFieldText = textField.getText();
+
+        if (editFieldText.isEmpty() || editFieldText.replace(" ","").isEmpty())
+            btn_send.setEnabled(false);
+        else
+            btn_send.setEnabled(true);
+    }
+
     public static void main(String[] args) {
         JFrame frame = new JFrame("ClientView");
         frame.setContentPane(new ClientView().JPanelArea);
@@ -24,4 +83,6 @@ public class ClientView extends JFrame {
         frame.pack();
         frame.setVisible(true);
     }
+
+
 }
