eb8ef078b442bd50420d47993ed8d5b7e5075e7b
breitenstein.alexander@googlemail.com <breitenstein.alexander@googlemail.com@b347f7b3-2220-c1e4-933a-b99727ceb3eb>
2013-04-24 19:47:59 +0000

    rnp2: gui zum zweiten mal gebaut -.-

--- trunk/rn-praktikum/src/rn/praktikum2/client/ClientView.java
+++ trunk/rn-praktikum/src/rn/praktikum2/client/ClientView.java
@@ -3,14 +3,10 @@ package rn.praktikum2.client;
 import javax.swing.*;
 import javax.swing.event.DocumentEvent;
 import javax.swing.event.DocumentListener;
-import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.event.InputMethodEvent;
-import java.awt.event.InputMethodListener;
 import java.beans.PropertyChangeEvent;
 import java.beans.PropertyChangeListener;
-import java.util.*;
 import java.util.List;
 
 /**
